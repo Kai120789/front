4674eb9cfbaff6ebb562f16aff7cc0d9a775ab2d
Kai120789 <zoomer120789@gmail.com>
2024-12-15 14:52:47 +0300

    fix: change class component button to func component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import classNames from 'classnames'
 
-class Button extends React.Component {
-    render() {
-        return <button className={`button ${this.props.outline? 'button--outline' : ''}`}>{this.props.children}</button>
-    }
+function Button(props) {
+    console.log(props)
+    return (
+        <button className={classNames('button', {
+            'button--outline': props.outline
+        })}>
+            {props.children}
+        </button>
+    )
 }
 
 export default Button
